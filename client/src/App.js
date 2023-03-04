b94982de980835cea7c2615393d4695b31445c2c
meenakshi singh <meenakshisinghthakur4668@gmail.com>
2023-03-04 17:33:30 +0530

    hackathon

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import AllRoutes from "./components/Routes/AllRoutes";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <AllRoutes />
+  
     </div>
   );
 }
